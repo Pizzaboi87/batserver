a23077a76c2d200b13d2e3728a99a71076e73e46
Peter Weiser <peterweiser87@gmail.com>
2023-01-02 18:25:09 +0200

    Cors settings.

--- routes/batServerMovies.js
+++ routes/batServerMovies.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors')
 const batServer = require('../services/batServerMovies');
+
+router.use(cors())
 
 router.get('/', async function(req, res, next) {
     try {
